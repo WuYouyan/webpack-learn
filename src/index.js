5efa288b950155d5d65d1a1d14348c8ab595b280
Youyan WU <wuyouyan1@gmail.com>
2020-08-04 21:18:20 +0200

    webpack : resolve         configuration of imported modules name         1. modules         2. alias         3. mainFiels         4. extensions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
+// import 'bootstrap/dist/css/bootstrap.css'; // 1
+// import 'bootstrap'; // 2,3 alias in webpack.config.js 
+// import './style.css'; // without extensions in webpack.config.js
+import './style'; // 4
+
 let xhr = new XMLHttpRequest();
 
-// http://localhost:8080 default,  webpack-dev-server -transfer request to -> 3000 
-// http-proxy
-//1
-// xhr.open('GET', 'http://localhost:8080/api/user', true);
-//2
-// xhr.open('GET', 'http://localhost:8080/user', true); // from before() in devServer of webpack.config.js
-                                                        // webpack server transfer http request by proxy to localhost:3000
-//3 browser: localhost:3000 will run index.js to get response from server.js (server port is webpack port because webpack is running on server )
 xhr.open('GET', '/user', true); // from before() in devServer of webpack.config.js
 
 xhr.onload = function() {
@@ -16,13 +13,3 @@ xhr.onload = function() {
 
 xhr.send();
 
-
-// console.log('index');
-
-// class Log {
-//     constructor(){
-//         console.log("Log -> constructor -> constructor");
-//     }
-// }
-
-// let log = new Log(); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
 
-// 1. clean-webpack-plugin // remove previous build automatically
-// 2. copy-webpack-plugin  // copy files to build directory
-// 3. banner-webpack-plugin (built-in) // add copy right
 module.exports = {
+    resolve:{
+        modules: [
+            path.resolve('node_modules'),
+            // path.resolve('dist') // 1
+        ],
+        // alias: { // 2
+        //     bootstrap: 'bootstrap/dist/css/bootstrap.css'
+        // }
+        // mainFields:['style', 'main'] // 3 in node_modules/ .../package.json
+        // mainFiles: [] // 4, entry file index.js by default
+        extensions: ['.js', '.css', '.json'] // 5 import 'style', will search at first style.js then style.css and then json, once find, return  
+    },
     devServer:{
         port: 8080,
         progress:true,
         contentBase: './dist',
         compress:true, //compress files gzip
-        // 1
-        // proxy: {
-        //     // '/api': 'http://localhost:3000' 
-        //     '/api': { //     
-        //         target: 'http://localhost:3000',
-        //         pathRewrite: {
-        //             'api': '' // will transfer api/user -> /user to http://localhost:3000
-        //         }
-        //     }
-        // },
-        // 2 mock data
-        // before(app){ // hooks 
-        //     app.get('/user', (req, res) => {
-        //         res.json({name: 'learn webpack - before'});
-        //     });
-        // }
-        // 3 server without using proxy, bootstrap webpack in server and port using server port
     },
     mode: 'development', 
     entry: {
@@ -42,6 +31,10 @@ module.exports = {
 
     module: {
         rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
             {
                 test:/\.js$/,
                 use: {
@@ -58,12 +51,5 @@ module.exports = {
             template: './index.html',
             filename: 'index.html',
         }),
-        // new CleanWebpackPlugin(), 
-        // new CopyWebpackPlugin({
-        //     patterns: [
-        //         { from: './doc', to: './doc' },
-        //       ],
-        // }),
-        // new webpack.BannerPlugin('Make 2019 by yy')
     ]
 }
